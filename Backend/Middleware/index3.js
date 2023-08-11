4b7e2304aefcec14a7494ad6f055c56413867ec8
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-08-11 21:04:01 +0800

    New backend project

--- Backend/Middleware/index3.js
+++ Backend/Middleware/index3.js
@@ -1,7 +1,13 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+function logger (req, res, next) {
+  console.log(req.method);
+  console.log(req.url);
+  next();
+}
 
 app.use(logger);
 
--- Backend/Middleware/index4.js
+++ Backend/Middleware/index4.js
@@ -1,7 +1,30 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 3001;
+var randomName = "";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function nameGenerator(req, res, next) {
+  randomName = req.body.street + req.body.pet;
+  next();
+}
+
+app.use(nameGenerator);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>Your band name is ${randomName}</h1>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
