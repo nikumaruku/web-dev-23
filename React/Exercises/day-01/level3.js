d97d3912318979a69d49282deb8654c5029e8ca5
Nik Syahmi <niksyahmiirfan01@gmail.com>
2023-08-28 20:39:08 +0800

    Import countries array to level03:day1

--- React/Exercises/day-01/level3.js
+++ React/Exercises/day-01/level3.js
@@ -1,3 +1,5 @@
+const countries = require("./level2/countries");
+/*PART 1*/
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
 
 //Sort the array and find the min and max age
@@ -7,7 +9,6 @@ const maxAge = ages[ages.length - 1];
 console.log(ages);
 
 //Find the median age(one middle item or two middle items divided by two)
-ages.sort();
 const middleIndex = Math.floor(ages.length / 2);
 if (ages.length % 2 === 1) {
   const median = ages[middleIndex];
@@ -26,7 +27,6 @@ const avgAge = sumAge / ages.length;
 console.log(avgAge);
 
 //Find the range of the ages(max minus min)
-ages.sort();
 const min = ages[0];
 const max = ages[ages.length - 1];
 const ageRange = max - min;
@@ -41,3 +41,6 @@ if (diffMin > diffMax)
 if (diffMin < diffMax)
   console.log(`The absolute difference (max - average) is larger: ${diffMax}`);
 else console.log("The absolute differences are equal.");
+
+/*PART 2*/
+//Slice the first ten countries from the countries array
