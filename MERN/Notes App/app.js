756b2dfa93197ee5b7f95f078cc5c6bd92c60720
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-08-29 23:28:41 +0800

    Create Mongoose Schema, Model

--- MERN/Notes App/app.js
+++ MERN/Notes App/app.js
@@ -1,29 +1,31 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
-
-app.get("/", function(req, res) {
-
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
+const itemSchema = mongoose.Schema({
+  name: String,
 });
 
-app.post("/", function(req, res){
+const items = mongoose.model("Items", itemSchema);
 
+const item1 = new items({ name: "Orange" });
+const item2 = new items({ name: "Apple" });
+const item3 = new items({ name: "Durian" });
+
+const collectionItems = [item1, item2, item3];
+
+app.get("/", function (req, res) {
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
   const item = req.body.newItem;
 
   if (req.body.list === "Work") {
@@ -35,14 +37,14 @@ app.post("/", function(req, res){
   }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
