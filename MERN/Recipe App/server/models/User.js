a07fb5078b244374fa595d5b0c2712d0469c5fca
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-09-01 15:49:56 +0800

    Debug connection issues to DB

--- MERN/Recipe App/server/models/User.js
+++ MERN/Recipe App/server/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const UserSchema = mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
--- MERN/Recipe App/server/routes/User.js
+++ MERN/Recipe App/server/routes/User.js
@@ -8,7 +8,16 @@ const router = express.Router();
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
   const user = await UserModel.findOne({ username: username });
-  res.json(user);
+
+  if (user) return res.json({ message: "User already exists!" });
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new UserModel({
+    username: username,
+    password: hashedPassword,
+  });
+  await newUser.save();
+  res.json({ message: "User has been successfully registered!" });
 });
 
 router.post("/login");
