36c8ee92fe2eba2dce2925ef8ccb1da20a09810d
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-09-02 01:09:40 +0800

    Implement Login + Register function

--- MERN/Recipe App/client/src/components/navbar.js
+++ MERN/Recipe App/client/src/components/navbar.js
@@ -1,12 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 export const Navbar = () => {
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setCookies("access_token", "");
+    navigate("/auth");
+  };
   return (
     <div className="navbar">
       <Link to="/">Home</Link>
       <Link to="/create-recipe">Create Recipe</Link>
       <Link to="/saved-recipe">Saved Recipe</Link>
-      <Link to="/auth">Login/Register</Link>
+      {!cookies.access_token ? (
+        <Link to="/auth">Login/Register</Link>
+      ) : (
+        <button onClick={logout}>Logout</button>
+      )}
     </div>
   );
 };
--- MERN/Recipe App/client/src/pages/auth.js
+++ MERN/Recipe App/client/src/pages/auth.js
@@ -1,3 +1,113 @@
+import { useState } from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+
 export const Auth = () => {
-  return <div>Authentication</div>;
+  return (
+    <div className="auth">
+      <Login />
+      <Register />
+    </div>
+  );
+};
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [_, setCookies] = useCookies(["access_token"]);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3001/auth/login", {
+        username,
+        password,
+      });
+      setCookies("access_token", response.data.token);
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+      alert("Something went wrong! Try again");
+    }
+  };
+
+  return (
+    <Form
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      label="Login"
+      onSubmit={handleLogin}
+    />
+  );
+};
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:3001/auth/register", {
+        username,
+        password,
+      });
+      alert("Registration successful! Please proceed to login");
+    } catch (err) {
+      console.error(err);
+      alert("Something went wrong! Try again");
+    }
+  };
+
+  return (
+    <Form
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      label="Register"
+      onSubmit={handleRegister}
+    />
+  );
+};
+
+const Form = ({
+  username,
+  setUsername,
+  password,
+  setPassword,
+  label,
+  onSubmit,
+}) => {
+  return (
+    <div className="auth-container">
+      <form onSubmit={onSubmit}>
+        <h2>{label}</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">{label}</button>
+      </form>
+    </div>
+  );
 };
