2cacd59cb397cb3c1dc2d8f35ac25b0835926a4d
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-09-01 15:35:30 +0800

    Add API route for register

--- MERN/Recipe App/server/src/index.js
+++ MERN/Recipe App/server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import { userRouter } from "../routes/User.js";
 
 const app = express();
 const port = 3001;
@@ -8,7 +9,11 @@ const port = 3001;
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://kaizen:M2qIY09vqDAwvysU@recipes.gtq6aa8.mongodb.net/recipes?retryWrites=true&w=majority");
+app.use("/auth", userRouter);
+
+mongoose.connect(
+  "mongodb+srv://kaizen:M2qIY09vqDAwvysU@recipes.gtq6aa8.mongodb.net/recipes?retryWrites=true&w=majority"
+);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
