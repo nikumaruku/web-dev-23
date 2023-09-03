20056d802017c855a40f1d79bb45ee7b5ed998f8
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-09-03 23:22:34 +0800

    Add middleware for user auth

--- MERN/Recipe App/server/routes/User.js
+++ MERN/Recipe App/server/routes/User.js
@@ -38,3 +38,16 @@ router.post("/login", async (req, res) => {
 });
 
 export { router as userRouter };
+
+//Middleware
+export const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, "secret", (error) => {
+      if (error) return res.sendStatus(403);
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
