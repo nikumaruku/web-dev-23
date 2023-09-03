1ac7daeead8236cb71245bdc3b853382ee898fdd
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-09-03 12:41:06 +0800

    Configure  Homepage

--- MERN/Recipe App/client/src/pages/home.js
+++ MERN/Recipe App/client/src/pages/home.js
@@ -1,4 +1,76 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID";
+
 export const Home = () => {
-    return <div>Home</div>;
+  const userID = useGetUserID();
+
+  const [recipes, setRecipes] = useState([]);
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/recipes");
+        setRecipes(response.data);
+      } catch (err) {
+        console.error(err);
+        alert("Something went wrong! Please try again");
+      }
+    };
+
+    const fetchSavedRecipe = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/recipes/savedRecipes/id/${userID}`
+        );
+        setSavedRecipes(response.data.savedRecipes);
+      } catch (err) {
+        console.error(err);
+        alert("Something went wrong! Please try again");
+      }
+    };
+
+    fetchRecipe();
+    fetchSavedRecipe();
+  }, []);
+
+  const saveRecipe = async (recipeID) => {
+    try {
+      const response = await axios.put("http://localhost:3001/recipes", {
+        recipeID,
+        userID,
+      });
+      setSavedRecipes(response.data.savedRecipes);
+    } catch (err) {
+      console.error(err);
+      alert("Something went wrong! Please try again");
+    }
   };
-  
+
+  return (
+    <div>
+      <h2>Recipes</h2>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+            </div>
+            <button
+              onClick={() => saveRecipe(recipe._id)}
+              disabled={savedRecipes.includes(recipe._id)}
+            >
+              {savedRecipes.includes(recipe._id) ? "Saved" : " Save"}
+            </button>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageURL} alt={recipe.name} />
+            <p>Cooking Time: {recipe.cookingTime} (minutes)</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
