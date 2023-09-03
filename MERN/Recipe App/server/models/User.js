b786199ee7016088249bf98f4a8b97d84bfadf0f
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-09-03 12:42:26 +0800

    Fix user schema

--- MERN/Recipe App/server/models/User.js
+++ MERN/Recipe App/server/models/User.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 const UserSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  savedRecipes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "recipes",
+  }],
 });
 
 export const UserModel = mongoose.model("users", UserSchema);
