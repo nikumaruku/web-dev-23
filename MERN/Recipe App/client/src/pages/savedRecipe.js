6915d1a4c19dcf73cde0bdb1ecabf550094b644f
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-09-03 12:41:49 +0800

    Configure Saved Recipe page

--- MERN/Recipe App/client/src/pages/savedRecipe.js
+++ MERN/Recipe App/client/src/pages/savedRecipe.js
@@ -1,3 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID";
+
 export const SavedRecipe = () => {
-  return <div>Saved Recipe</div>;
+  const userID = useGetUserID();
+
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchSavedRecipe = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/recipes/savedRecipes/${userID}`
+        );
+        setSavedRecipes(response.data.savedRecipes);
+      } catch (err) {
+        console.error(err);
+        alert("Something went wrong! Please try again");
+      }
+    };
+
+    fetchSavedRecipe();
+  }, []);
+
+  return (
+    <div>
+      <h2>Here are your Recipes!</h2>
+      <ul>
+        {savedRecipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageURL} alt={recipe.name} />
+            <p>Cooking Time: {recipe.cookingTime} (minutes)</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
