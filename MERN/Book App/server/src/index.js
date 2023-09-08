b704d63be012e678cf49c48c02681e4bed4c6f74
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-09-08 14:38:25 +0800

    Refactor book routes to seperate directory

--- MERN/Book App/server/src/index.js
+++ MERN/Book App/server/src/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
-import { BookModel } from "../models/book.js";
+import { bookRouter } from "../router/bookRouter.js";
 
 const app = express();
 const port = 3001;
 
 //Express middleware
 app.use(express.json());
+
+//Routes
+app.use("/books", bookRouter);
 
 //MongoDB connection configuration
 mongoose
@@ -20,93 +23,7 @@ mongoose
     console.log(err);
   });
 
-//--------> Routes
-
-//Create new book
-app.post("/books", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res
-        .status(400)
-        .json({ error: "Please fill in all the book details!" });
-    }
-
-    const newBook = {
-      title: req.body.title,
-      author: req.body.author,
-      publishYear: req.body.publishYear,
-    };
-
-    const book = await BookModel.create(newBook);
-
-    return res.status(201).json(book);
-  } catch (err) {
-    console.error("Error creating a book:", err);
-    return res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-//Get all books
-app.get("/books", async (req, res) => {
-  try {
-    const allBooks = await BookModel.find({});
-    res.status(200).json(allBooks);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching book data" });
-  }
-});
-
-//Get book by ID
-app.get("/books/:id", async (req, res) => {
-  try {
-    const bookID = req.params.id;
-    const book = await BookModel.findById({ _id: bookID });
-    res.status(200).json(book);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching book requested!" });
-  }
-});
-
-//Update book information
-app.put("/books/:id", async (req, res) => {
-  try {
-    const bookID = req.params.id;
-
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res
-        .status(400)
-        .json({ error: "Please fill in all the book details!" });
-    }
-
-    const updatedBookInfo = {
-      title: req.body.title,
-      author: req.body.author,
-      publishYear: req.body.publishYear,
-    };
-
-    const book = await BookModel.findByIdAndUpdate(
-      { _id: bookID },
-      updatedBookInfo
-    );
-    res
-      .status(200)
-      .json({ message: "Book info has been successfully updated!" });
-  } catch (error) {
-    res.status(500).json({ message: "Error updating book information!" });
-  }
-});
-
-//Delete book based on ID
-app.delete("/books/:id", async (req, res) => {
-  try {
-    const bookID = req.params.id;
-    const book = await BookModel.findOneAndDelete({ _id: bookID });
-    res.status(200).json({ message: "Book has been successfully deleted!" });
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching book requested!" });
-  }
-});
-
+//Listen to port
 app.listen(port, (req, res) => {
   console.log(`Server is running on port ${port}`);
 });
